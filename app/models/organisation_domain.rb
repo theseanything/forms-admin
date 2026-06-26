678e1ea859e38d498e49ae1abd9c47e3cab261d1
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-06-26 15:58:37 +0100

    Add OrganisationDomain model

---
+++ app/models/organisation_domain.rb
@@ -0,0 +1,8 @@
+class OrganisationDomain < ApplicationRecord
+  belongs_to :organisation
+
+  validates :domain, presence: true
+  validates :domain, uniqueness: { scope: :organisation_id, case_sensitive: false }
+
+  before_validation -> { self.domain = domain&.downcase&.strip }
+end
